815641c36cc7255d907fd1b08d1ff72d6bb92cca
hemant <hemantrayte25@gmail.com>
2025-09-04 10:08:43 +0530

    basic express app

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
-const port = 3000
+
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app
+app.get('/twitter', (req, res) => {
+  res.send('hemant rayte')
+})
+
+app.get("/login", (req, res) => {
+  res.send('<h1>please log in || hemant rayte</h2>')
+})
+
+app.get("/signup", (req, res) => {
+  res.send('<h1>please sign in || hemant rayte</h2>')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
